6e108d08ff7c88e1d356d7b7c72eb2a732cd18dc
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2023-10-25 19:31:36 +0200

    Update resnet_model.py

--- src/models/resnet_model.py
+++ src/models/resnet_model.py
@@ -4,46 +4,42 @@ import torch.optim as optim
 import torchvision
 from torchvision import transforms, models
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 from PIL import Image
 import os
-
 from torchvision import datasets
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torchvision.transforms as transforms
+import timm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from tqdm.notebook import tqdm
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from torchvision.models import ResNet50_Weights
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import csv
+import datetime
 
 
-class ResNet_baseline(nn.Module):
-    def __init__(self, num_diseases=8):
-        super(ResNet_baseline, self).__init__()
-        self.resnet = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
-        self.fc1 = nn.Linear(2 * 1000, 256)
-        self.fc2 = nn.Linear(256, num_diseases)
+class VisionTransformer(nn.Module):
+    def __init__(self, num_classes=8):
+        super(VisionTransformer, self).__init__()
+        self.num_classes = num_classes
 
-    def forward(self, img_left, img_right):
-        x_left = self.resnet(img_left)
-        x_right = self.resnet(img_right)
-        x = torch.cat((x_left, x_right), dim =1)
-        x = nn.ReLU()(self.fc1(x))
-        x = self.fc2(x)
-        #x = torch.sigmoid(self.fc2(x))
-        return x
+        # Vision Transformer backbone
+        self.backbone = timm.create_model('vit_base_patch16_224', pretrained=True)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-model = ResNet_baseline().to(device)
-for param in model.resnet.parameters():
-   param.requires_grad = False
+        # Replace the final classification layer
+        self.backbone.head = nn.Identity()
 
-trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"Number of trainable parameters: {trainable_params}")
+        # Add a classification head for multi-label classification
+        self.classification_head = nn.Linear(self.backbone.embed_dim * 2, num_classes)
 
-# from efficientnet paper
-loss_weight = torch.tensor([1, 1.2, 1.5, 1.5, 1.5, 1.5, 1.5, 1.2]).to(device)
-criterion = nn.BCEWithLogitsLoss(pos_weight=loss_weight)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    def forward(self, image_left, image_right):
+        features_left = self.backbone(image_left)
+        features_right = self.backbone(image_right)
+        combined_features = torch.cat((features_left, features_right), dim=1)
+
+        # Pass through the classification head
+        logits = self.classification_head(combined_features)
+
+        return logits
