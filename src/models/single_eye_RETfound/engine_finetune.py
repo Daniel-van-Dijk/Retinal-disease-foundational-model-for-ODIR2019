be1b7b55c626c206840f17674d608bb0fb7f8cae
Daniël van Dijk <47952200+Daniel-van-Dijk@users.noreply.github.com>
2023-10-26 21:34:56 +0200

    Add files via upload

---
+++ src/models/single_eye_RETfound/engine_finetune.py
@@ -0,0 +1,257 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# Partly revised by YZ @UCL&Moorfields
+# --------------------------------------------------------
+
+import math
+import sys
+import csv
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.data import Mixup
+from timm.utils import accuracy
+from typing import Iterable, Optional
+import util.misc as misc
+import util.lr_sched as lr_sched
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, average_precision_score,multilabel_confusion_matrix
+from pycm import *
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+
+
+
+def misc_measures(confusion_matrix):
+    
+    acc = []
+    sensitivity = []
+    specificity = []
+    precision = []
+    G = []
+    F1_score_2 = []
+    mcc_ = []
+    
+    for i in range(1, confusion_matrix.shape[0]):
+        cm1=confusion_matrix[i]
+        acc.append(1.*(cm1[0,0]+cm1[1,1])/np.sum(cm1))
+        sensitivity_ = 1.*cm1[1,1]/(cm1[1,0]+cm1[1,1])
+        sensitivity.append(sensitivity_)
+        specificity_ = 1.*cm1[0,0]/(cm1[0,1]+cm1[0,0])
+        specificity.append(specificity_)
+        precision_ = 1.*cm1[1,1]/(cm1[1,1]+cm1[0,1])
+        precision.append(precision_)
+        G.append(np.sqrt(sensitivity_*specificity_))
+        F1_score_2.append(2*precision_*sensitivity_/(precision_+sensitivity_))
+        mcc = (cm1[0,0]*cm1[1,1]-cm1[0,1]*cm1[1,0])/np.sqrt((cm1[0,0]+cm1[0,1])*(cm1[0,0]+cm1[1,0])*(cm1[1,1]+cm1[1,0])*(cm1[1,1]+cm1[0,1]))
+        mcc_.append(mcc)
+        
+    acc = np.array(acc).mean()
+    sensitivity = np.array(sensitivity).mean()
+    specificity = np.array(specificity).mean()
+    precision = np.array(precision).mean()
+    G = np.array(G).mean()
+    F1_score_2 = np.array(F1_score_2).mean()
+    mcc_ = np.array(mcc_).mean()
+    
+    return acc, sensitivity, specificity, precision, G, F1_score_2, mcc_
+
+
+
+
+
+def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    mixup_fn: Optional[Mixup] = None, log_writer=None,
+                    args=None):
+    model.train(True)
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+
+    accum_iter = args.accum_iter
+
+    optimizer.zero_grad()
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.log_dir))
+
+    for data_iter_step, (img, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        #img_left = img_left.to(device, non_blocking=True)
+        img = img.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        if mixup_fn is not None:
+            samples, targets = mixup_fn(samples, targets)
+        with torch.cuda.amp.autocast():
+            outputs = model(img)
+            loss = criterion(outputs, targets)
+
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        loss /= accum_iter
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=False,
+                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize()
+
+        metric_logger.update(loss=loss_value)
+        min_lr = 10.
+        max_lr = 0.
+        for group in optimizer.param_groups:
+            min_lr = min(min_lr, group["lr"])
+            max_lr = max(max_lr, group["lr"])
+
+        metric_logger.update(lr=max_lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
+            log_writer.add_scalar('lr', max_lr, epoch_1000x)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+
+
+@torch.no_grad()
+def evaluate_old(data_loader, model, device, task, epoch, mode, num_class):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+    
+    if not os.path.exists(task):
+        os.makedirs(task)
+
+    prediction_decode_list = []
+    prediction_list = []
+    true_label_decode_list = []
+    true_label_onehot_list = []
+    
+    # switch to evaluation mode
+    model.eval()
+
+    for batch in metric_logger.log_every(data_loader, 10, header):
+        images = batch[0]
+        target = batch[-1]
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+        true_label=F.one_hot(target.to(torch.int64), num_classes=num_class)
+
+        # compute output
+        with torch.cuda.amp.autocast():
+            output = model(images)
+            loss = criterion(output, target)
+            prediction_softmax = nn.Softmax(dim=1)(output)
+            _,prediction_decode = torch.max(prediction_softmax, 1)
+            _,true_label_decode = torch.max(true_label, 1)
+
+            prediction_decode_list.extend(prediction_decode.cpu().detach().numpy())
+            true_label_decode_list.extend(true_label_decode.cpu().detach().numpy())
+            true_label_onehot_list.extend(true_label.cpu().detach().numpy())
+            prediction_list.extend(prediction_softmax.cpu().detach().numpy())
+
+        acc1,_ = accuracy(output, target, topk=(1,2))
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+    # gather the stats from all processes
+    true_label_decode_list = np.array(true_label_decode_list)
+    prediction_decode_list = np.array(prediction_decode_list)
+    confusion_matrix = multilabel_confusion_matrix(true_label_decode_list, prediction_decode_list,labels=[i for i in range(num_class)])
+    acc, sensitivity, specificity, precision, G, F1, mcc = misc_measures(confusion_matrix)
+    
+    auc_roc = roc_auc_score(true_label_onehot_list, prediction_list,multi_class='ovr',average='macro')
+    auc_pr = average_precision_score(true_label_onehot_list, prediction_list,average='macro')          
+            
+    metric_logger.synchronize_between_processes()
+    
+    print('Sklearn Metrics - Acc: {:.4f} AUC-roc: {:.4f} AUC-pr: {:.4f} F1-score: {:.4f} MCC: {:.4f}'.format(acc, auc_roc, auc_pr, F1, mcc)) 
+    results_path = task+'_metrics_{}.csv'.format(mode)
+    with open(results_path,mode='a',newline='',encoding='utf8') as cfa:
+        wf = csv.writer(cfa)
+        data2=[[acc,sensitivity,specificity,precision,auc_roc,auc_pr,F1,mcc,metric_logger.loss]]
+        for i in data2:
+            wf.writerow(i)
+            
+    
+    if mode=='test':
+        cm = ConfusionMatrix(actual_vector=true_label_decode_list, predict_vector=prediction_decode_list)
+        cm.plot(cmap=plt.cm.Blues,number_label=True,normalized=True,plot_lib="matplotlib")
+        plt.savefig(task+'confusion_matrix_test.jpg',dpi=600,bbox_inches ='tight')
+    
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()},auc_roc
+
+
+
+
+#calculate kappa, F-1 socre and AUC value
+def ODIR_Metrics(gt_data, pr_data):
+    """ function from ODIR2019 challenge """
+    th = 0.5
+    gt = gt_data.flatten()
+    pr = pr_data.flatten()
+    kappa = metrics.cohen_kappa_score(gt, pr>th)
+    f1 = metrics.f1_score(gt, pr>th, average='micro')
+    auc = metrics.roc_auc_score(gt, pr)
+    final_score = (kappa+f1+auc)/3.0
+
+    precisions = metrics.precision_score(gt_data, pr_data, average=None)
+    recalls = metrics.recall_score(gt_data, pr_data, average=None)
+    classes_f1 = metrics.f1_score(gt_data, pr_data, average=None)
+    for class_index in range(len(precisions)):
+        print(f'Class {class_index + 1}: F1 = {classes_f1[class_index]}, Precision = {precisions[class_index]}, Recall = {recalls[class_index]}')
+
+    return kappa, f1, auc, final_score
+
+@torch.no_grad()
+def evaluate(model, dataloader, device, criterion):
+    model.eval()
+    all_labels = []
+    all_logits = []
+    val_loss = 0
+    with torch.no_grad():
+        for (images, labels) in dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            logits = model(images)
+            loss = criterion(logits, labels)
+            val_loss += loss.item()
+            all_labels.append(labels.cpu().numpy())
+            all_logits.append(logits.detach().cpu().numpy())
+
+    all_labels = np.vstack(all_labels)
+    all_logits = np.vstack(all_logits)
+
+    all_preds = (all_logits > 0.5).astype(np.float32)
+    # val_loss, average_score, kappa, f1, auc, val_confusion, val_accuracy
+    kappa, f1, auc, final_score = ODIR_Metrics(all_labels, all_preds)
+
+    return val_loss / len(dataloader), final_score, kappa, f1, auc
---
+++ src/models/single_eye_RETfound/generate_predictions_csv.py
@@ -0,0 +1,260 @@
+import argparse
+import datetime
+import json
+import numpy as np
+import os
+import time
+from pathlib import Path
+import pandas as pd
+
+import torch
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, models
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import os
+
+
+
+
+from torchvision import datasets
+from tqdm.notebook import tqdm
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+import csv
+import torch.nn.functional as F
+
+import timm
+
+assert timm.__version__ == "0.3.2" # version check
+from timm.models.layers import trunc_normal_
+from timm.data.mixup import Mixup
+from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+from sklearn.model_selection import train_test_split, GroupShuffleSplit
+
+import util.lr_decay as lrd
+import util.misc as misc
+from util.datasets import build_dataset, ODIRDataset, TestDataset
+from util.pos_embed import interpolate_pos_embed
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+
+import models_vit
+from ODIR_model import ODIRmodel
+
+from engine_finetune import train_one_epoch, evaluate
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE fine-tuning for image classification', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    # Model parameters
+    parser.add_argument('--model', default='vit_large_patch16', type=str, metavar='MODEL',
+                        help='Name of model to train')
+
+    parser.add_argument('--input_size', default=224, type=int,
+                        help='images input size')
+
+    parser.add_argument('--drop_path', type=float, default=0.1, metavar='PCT',
+                        help='Drop path rate (default: 0.1)')
+
+    # Optimizer parameters
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--layer_decay', type=float, default=0.75,
+                        help='layer-wise lr decay from ELECTRA/BEiT')
+
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=10, metavar='N',
+                        help='epochs to warmup LR')
+
+    # Augmentation parameters
+    parser.add_argument('--color_jitter', type=float, default=None, metavar='PCT',
+                        help='Color jitter factor (enabled only when not using Auto/RandAug)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                        help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
+    parser.add_argument('--smoothing', type=float, default=0.1,
+                        help='Label smoothing (default: 0.1)')
+
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                        help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel',
+                        help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1,
+                        help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', action='store_true', default=False,
+                        help='Do not random erase first (clean) augmentation split')
+
+    # * Mixup params
+    parser.add_argument('--mixup', type=float, default=0,
+                        help='mixup alpha, mixup enabled if > 0.')
+    parser.add_argument('--cutmix', type=float, default=0,
+                        help='cutmix alpha, cutmix enabled if > 0.')
+    parser.add_argument('--cutmix_minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup_prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    parser.add_argument('--mixup_switch_prob', type=float, default=0.5,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup_mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+
+    # * Finetuning params
+    parser.add_argument('--finetune', default='',type=str,
+                        help='finetune from checkpoint')
+    parser.add_argument('--task', default='',type=str,
+                        help='finetune from checkpoint')
+    parser.add_argument('--global_pool', action='store_true')
+    parser.set_defaults(global_pool=True)
+    parser.add_argument('--cls_token', action='store_false', dest='global_pool',
+                        help='Use class token instead of global pool for classification')
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='/home/jupyter/Mor_DR_data/data/data/IDRID/Disease_Grading/', type=str,
+                        help='dataset path')
+    parser.add_argument('--nb_classes', default=8, type=int,
+                        help='number of the classification types')
+
+    parser.add_argument('--output_dir', default='./output_dir',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./output_dir',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', action='store_true',
+                        help='Perform evaluation only')
+    parser.add_argument('--dist_eval', action='store_true', default=False,
+                        help='Enabling distributed evaluation (recommended during training for faster monitor')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+
+def main(args):
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = True
+    
+    test_loader = DataLoader(TestDataset('/home/scur0556/ODIR2019/data/cropped_ODIR-5K_Testing_Images', is_train=False, args=args), batch_size=16, shuffle=False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+    print(device)
+
+
+    base_vit_model = models_vit.__dict__[args.model](
+        num_classes=args.nb_classes,
+        drop_path_rate=args.drop_path,
+        global_pool=args.global_pool,
+    )
+
+    model = ODIRmodel(base_vit_model=base_vit_model, num_classes=args.nb_classes)
+    model.to(device)
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+    checkpoint_path = '/home/scur0556/ODIR2019/src/models/foundational_model/run_4193859/best_score_checkpoint.pth'
+    checkpoint = torch.load(checkpoint_path, map_location=device) 
+    state_dict = checkpoint['model_state_dict']
+    model_sd_keys = set(model.state_dict().keys())
+    ckpt_sd_keys = set(state_dict.keys())
+
+    missing_keys = model_sd_keys - ckpt_sd_keys
+    unexpected_keys = ckpt_sd_keys - model_sd_keys
+
+    print(f"Missing keys: {missing_keys}")
+    print(f"Unexpected keys: {unexpected_keys}")
+    #if any(k.startswith("module.") for k in state_dict.keys()):
+    #    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+
+    # Load the state_dict into the model
+    model.load_state_dict(state_dict)
+
+    model.eval()
+
+    # check logit_output param
+    save_predictions(model, test_loader, device, 'prob_predictions_random.csv', logit_output=True)
+    #save_predictions(model, test_loader, device, 'logit_predictions.csv', logit_output=False)
+
+
+def save_predictions(model, dataloader, device, output_file, logit_output=True):
+
+    model.eval()
+    all_ids = []
+    all_probs = []
+
+
+
+    with torch.no_grad():
+        for (images_left, images_right, image_ids) in dataloader:
+            images_left, images_right = images_left.to(device), images_right.to(device)
+
+            output = model(images_left, images_right)
+            # set logit_output = True when model outputs logits so NO sigmoid applied in forward of model
+            # set logit_output = False when model outputs probs
+            if logit_output:
+              output = F.sigmoid(output)  # Convert logits to probabilities
+
+            all_ids.extend(image_ids.cpu().numpy())
+            all_probs.append(output.cpu().numpy())
+
+    all_probs = np.vstack(all_probs)
+
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['ID', 'N', 'D', 'G', 'C', 'A', 'H', 'M', 'O'])
+
+        for idx, prob in zip(all_ids, all_probs):
+            writer.writerow([idx] + list(prob))
+
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    main(args)
